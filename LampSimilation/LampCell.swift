7e1d3f81dbb4e4b262ca34829e8f09daa1bc623d
victorgubskih <112268920+victorgubskih@users.noreply.github.com>
2023-01-28 11:57:26 +0200

    create lampcell, constrain lamp switch

---
+++ LampSimilation/LampCell.swift
@@ -0,0 +1,16 @@
+//
+//  Lampsell.swift
+//  LampSimilation
+//
+//  Created by viktor on 28.01.2023.
+//
+
+import UIKit
+
+class LampCell: UITableViewCell {
+
+    @IBOutlet weak var iconView: UIImageView!
+    
+    @IBOutlet weak var switchView: UISwitch!
+    
+}
